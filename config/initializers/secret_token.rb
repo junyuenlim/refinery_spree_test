74c9df3b32e56e66097fd57f7dd24b5e0b5fcb5a
Jun Lim <junyuen@gmail.com>
2014-07-09 17:30:40 +0800

    Install RefineryCMS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefinerySpreeTest::Application.config.secret_token = 'b80391759e5db4c7c30948293d3082457452d78a3d23088cab9034ebe74440a1c2a8eb92f6279cfb5f8d0dc30a3d942b00060195d1773997ca76b0adbc8855c1'
